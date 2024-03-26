41e61ba54a9f808175562b0494295885f9d1b53a
GOLVEN666 <achraf.xchahid@gmail.com>
2024-03-26 18:53:01 +0000

    Add unit tests for all files, classes, and functions

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Defines unittests for console.py."""
+
+import unittest
+from console import HBNBCommand
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """Unittests for the HBNBCommand class."""
+
+    def test_prompt(self):
+        """Test if the prompt is set correctly."""
+        console = HBNBCommand()
+        self.assertEqual("(hbnb) ", console.prompt)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
